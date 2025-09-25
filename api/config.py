5e91a28dba259252979db1d64760a6c06082c7c1
maxunbearable <max.unbearable@gmail.com>
2025-09-25 14:52:24 +0300

    Add B2 configuration options and update requirements
    Added optional configuration parameters for B2 integration in the GlobalConfig class, including B2_KEY_ID, B2_APPLICATION_KEY, and B2_BUCKET_NAME. Updated requirements.txt to include aiofiles and b2sdk for enhanced functionality.

--- api/config.py
+++ api/config.py
@@ -18,6 +18,9 @@ class GlobalConfig(BaseConfig):
     MAILGUN_API_KEY: Optional[str] = None
     MAILGUN_DOMAIN: Optional[str] = None
     SECRET_KEY: Optional[str] = None
+    B2_KEY_ID: Optional[str] = None
+    B2_APPLICATION_KEY: Optional[str] = None
+    B2_BUCKET_NAME: Optional[str] = None
 
 
 class DevConfig(GlobalConfig):
